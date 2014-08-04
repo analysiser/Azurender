a4668e25f20654f04a4ebcc44c0ff49265cd660c
Xiao Li <xiao.li.cmu@gmail.com>
2014-08-04 19:39:16 -0400

    Added reflection file, for definition of Fresnel Equation, reflection/refretion models, (Especially for microfacet at this point, following PBRT convention)

---
+++ src/raytracer/azReflection.cpp
@@ -0,0 +1,14 @@
+//
+//  azReflection.cpp
+//  Azurender
+//
+//  Created by Xiao Li on 8/4/14.
+//
+//
+
+#include "azReflection.hpp"
+
+
+namespace _462 {
+    
+}
---
+++ src/raytracer/azReflection.hpp
@@ -0,0 +1,17 @@
+//
+//  azReflection.cpp
+//  Azurender
+//
+//  Created by Xiao Li on 8/4/14.
+//
+//
+
+#ifndef __Azurender__azReflection__
+#define __Azurender__azReflection__
+
+namespace _462 {
+    
+}
+
+
+#endif /* defined(__Azurender__azReflection__) */

768763e24407644932be2d32cd999a77d57f462c
Xiao Li <xiao.li.cmu@gmail.com>
2014-09-07 23:08:40 -0400

    Added files for constants and sampling functions in montecarlo.hpp/cpp. based on part

---
+++ src/raytracer/constants.h
@@ -0,0 +1,14 @@
+//
+//  constants.h
+//  Azurender
+//
+//  Created by Xiao Li on 9/7/14.
+//
+//
+
+namespace _462 {
+    
+#define INV_PI      (0.31830988618379067154f)
+#define INV_TWOPI   (0.15915494309189533577f)
+    
+}
---
+++ src/raytracer/montecarlo.hpp
@@ -0,0 +1,27 @@
+//
+//  montecarlo.h
+//  Azurender
+//
+//  Created by Xiao Li on 9/7/14.
+//
+//
+
+#include <iostream>
+
+#include "math/math.hpp"
+#include "math/vector.hpp"
+
+
+
+namespace _462 {
+    
+    void ConcentricSampleDisk(float u1, float u2, float *dx, float *dy);
+    
+    inline Vector3 CosineSampleHemisphere(float u1, float u2) {
+        
+        float x, y, z;
+        ConcentricSampleDisk(u1, u2, &x, &y);
+        z = std::sqrtf(std::max(0.f, 1.f - x * x - y * y));
+        return Vector3(x, y, z);
+    }
+}
